c98969bd04f08becc295cb986039492eb1685702
louiskizos <louisnyembo2@gmail.com>
2025-12-14 10:45:46 +0200

    conf

--- proof_capacity/settings.py
+++ proof_capacity/settings.py
@@ -115,7 +115,7 @@ CORS_ALLOW_HEADERS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',  # Doit être présent
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
