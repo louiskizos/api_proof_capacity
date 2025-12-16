1d531d9fdd5b1288c5dd60420fcae3c9cd8043bf
louiskizos <louisnyembo2@gmail.com>
2025-12-16 09:32:57 +0200

    videos

--- proof_capacity/settings.py
+++ proof_capacity/settings.py
@@ -13,7 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ======================
 SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
 
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+#DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "api-proof-capacity.onrender.com",
